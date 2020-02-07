bba1553a286e30fc63d5a32411db39e1d0d2cfe6
notebook <mraortiz@hotmail.com>
2020-02-07 05:56:09 -0300

    added search bar/buttons done/next gifs container

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ document.addEventListener("DOMContentLoaded", () => {
     const themesContainer = document.getElementById("themes-container");
 
     themesContainer.addEventListener("click", event => {
-        const themes = document.getElementById("names");
+        const themes = document.getElementById("themes");
 
-        if (event.target.id === "names" || "dropdownArrow" || "elegir") {
+        if (event.target.id === "themes" || "dropdownArrow" || "elegir") {
             themes.classList.toggle("show");
         } else {
             themes.classList.remove("show");
