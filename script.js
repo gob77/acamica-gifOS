6869c83c4cae1e1b811d955af9b8190bde1c4a9b
notebook <mraortiz@hotmail.com>
2020-02-13 06:00:24 -0300

    added suggested gifs/gifs container and styles

--- script.js
+++ script.js
@@ -1,32 +1,89 @@
 document.addEventListener("DOMContentLoaded", () => {
     const themesContainer = document.getElementById("themes-container");
-    const imput = document.getElementById("search");
+    const input = document.getElementById("search");
     const API_KEY = "wi0q59XfKI285mmhSNvmSSBSBdNi8NSo";
-    /* const API_URL = `http://api.giphy.com/v1/gifs/search?q=${imput}&api_key=${API_KEY}`; */
     const searchBTN = document.getElementById("search-btn");
-    let test;
+    const suggested = document.getElementById("suggested-container");
+    const trends = document.getElementById("trends-container");
+
+    /* get suggested gifs */
+
+    async function getRandoms(n) {
+        let data = await fetch(`http://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`);
+        let parsedData = await data.json();
+        /* console.log(parsedData); */
+
+        let container = document.createElement("div");
+        let title = document.createElement("div");
+        let titleContainer = document.createElement("h2");
+        let img = document.createElement("img");
+        let close = document.createElement("img");
+        let name = document.createTextNode(`#Imagen nro ${n}`);
+
+        container.setAttribute("class", "suggested-img");
+        title.setAttribute("class", "img-title");
+
+        img.src = parsedData.data.images.fixed_height.url;
+        img.setAttribute("class", "suggested-img");
+
+        close.src = "./img/close.svg";
+        close.setAttribute("class", "close");
+
+        title.appendChild(titleContainer);
+        title.appendChild(close);
+        titleContainer.appendChild(name);
+        container.appendChild(title);
+        container.appendChild(img);
+        suggested.appendChild(container);
+
+        if (n < 1) {
+            return;
+        } else if (n === 1) {
+            return 1;
+        }
+
+        return getRandoms(n - 1);
+    }
+
+    getRandoms(4);
+
+    /* get trending on load */
+
+    async function getTrends() {
+        let data = await fetch(`http://api.giphy.com/v1/gifs/trending?api_key=${API_KEY}&limit=24`);
+        let parsedData = await data.json();
+
+        console.log(parsedData);
+    }
+
+    getTrends();
+
+    /* get gifs on search */
 
     searchBTN.addEventListener("click", async () => {
-        let value = imput.value;
-        console.log(value);
+        /* get the data from giphy */
 
-        let data = await fetch(`http://api.giphy.com/v1/gifs/search?q=${value}&api_key=${API_KEY}`);
+        let value = input.value;
+
+        let data = await fetch(`http://api.giphy.com/v1/gifs/search?q=${value}&api_key=${API_KEY}&limit=5`);
         let parsedData = await data.json(data);
-        test = parsedData;
 
-        let container = document.getElementById("img-container");
-        debugger;
+        console.log(parsedData);
 
-        test.data.forEach((data, index) => {
-            /* console.log(data.images.original.url); */
+        /* put data into the DOM */
+
+        parsedData.data.forEach((data, index) => {
             let img = document.createElement("img");
             img.src = data.images.fixed_height.url;
-            container.appendChild(img);
+            trends.appendChild(img);
+
             img.addEventListener("error", () => {
                 console.log(`loading error on img ${index} `);
             });
         });
     });
+
+    /* THEMES */
 
     themesContainer.addEventListener("click", event => {
         const themes = document.getElementById("themes");
@@ -37,20 +94,4 @@ document.addEventListener("DOMContentLoaded", () => {
             themes.classList.remove("show");
         }
     });
-
-    /* async function getGifs() {
-        let data = await fetch(API_URL);
-        let parsedData = await data.json();
-        console.log(parsedData);
-        
-        placeIMG(parsedData);
-    } */
-
-    /* function placeIMG(data) {
-        console.log(data.data[0].images);
-
-        test.src = data.data[0].images.original.url;
-    }
-
-    getGifs(); */
 });
